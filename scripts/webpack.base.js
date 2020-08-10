5635992e4f70af1ffb413fc54220ac7f6f440260
lovely-upadhyay <lovely.upadhyay16@gmail.com>
2020-08-10 19:42:18 +0530

    Added webpack script to build

---
+++ scripts/webpack.base.js
@@ -0,0 +1,29 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            '@babel/preset-react',
+            [
+              '@babel/env',
+              {
+                targets: {
+                  browsers: ['last 2 versions']
+                }
+              }
+            ]
+          ],
+          "plugins": [
+            [
+              "@babel/plugin-proposal-class-properties"
+            ]
+          ]
+        }
+      }
+    ]
+  }
+}
---
+++ scripts/webpack.server.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const merge = require('webpack-merge')
+const webpackNodeExternals = require('webpack-node-externals')
+const baseConfig = require('./webpack.base.js')
+
+const config = {
+  target: 'node',
+  mode: 'production',
+  entry: path.resolve(__dirname, '../app/index.js'),
+  externals: [webpackNodeExternals()],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../build')
+  },
+  devServer: {
+    open: true
+  }
+}
+
+module.exports = merge(baseConfig, config)
